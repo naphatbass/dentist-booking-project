e664f3713a29c144e4da562de3f381061eab451e
naphatbass <naphatbass@outlook.com>
2024-11-24 15:54:47 +0700

    Update TopMenu.tsx

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './topmenu.module.css';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 
 export default async function TopMenu() {
 
